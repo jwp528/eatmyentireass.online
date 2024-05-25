ac4d712e810d3e08f12c81340e1652e07de3f342
Josh Parsons <jparsons@dragoncore.ca>
2024-05-25 00:15:01 -0300

    Added multiple asses, added breakdown

---
+++ Client/Pages/Home.razor.cs
@@ -0,0 +1,170 @@
+using BlazorApp.Shared;
+using BlazorBootstrap;
+using Microsoft.JSInterop;
+using static BlazorApp.Shared.Assets;
+using Timer = System.Timers.Timer;
+
+namespace BlazorApp.Client.Pages;
+
+public partial class Home
+{
+    Modal HelpDialog;
+    Modal AboutDialog;
+    Modal ResultsDialog;
+
+    bool gamePlaying;
+    Timer GameTimer;
+    int GameTimeInSeconds = 60; // 1 minute
+    string TimerDisplay => $"{GameTimeInSeconds / 60:D2}:{GameTimeInSeconds % 60:D2}";
+
+    double assesEaten = 0;
+    int piecesEaten = 0;
+
+    // breakdown
+    Dictionary<AssTypeEnum, int> Breakdown = new()
+    {
+        { AssTypeEnum.Boney, 0 },
+        { AssTypeEnum.Cartoon, 0 },
+        { AssTypeEnum.Flat, 0 },
+        { AssTypeEnum.Golden, 0 },
+        { AssTypeEnum.GYAT, 0 },
+        { AssTypeEnum.Hairy, 0 }
+    };
+    
+    AssTypeEnum CurrentAssType;
+    List<string> AssFrames = new();
+
+    string scoreText => assesEaten switch
+    {
+        0 => "Come on, don't knock it till you try it.",
+        >= 100 => $"Legendary! You've reached the pinnacle of ass-eating with {Math.Truncate(assesEaten)} or more! You're a true champion!",
+        >= 75 => $"Incredible! {Math.Truncate(assesEaten)} asses devoured! You're in a league of your own!",
+        >= 50 => $"Fantastic! You've eaten {Math.Truncate(assesEaten)} asses! That's a monumental achievement!",
+        >= 40 => $"Amazing! {Math.Truncate(assesEaten)} asses down the hatch! You're unstoppable!",
+        >= 30 => $"{Math.Truncate(assesEaten)} rumps! did you even taste them?",
+        >= 20 => $"{Math.Truncate(assesEaten)} asses. A hearty meal.",
+        >= 10 => $"{Math.Truncate(assesEaten)} asses eaten! Maybe you're just not hungry right now",
+        >= 5 => $"Only {Math.Truncate(assesEaten)} asses? It's not for everyone, at least you tried.",
+        _ => "How did you even get this? I default the number to 0..."
+    };
+
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+        GetNewAss();
+    }
+
+    void GetNewAss()
+    {
+        CurrentAssType = Assets.GetRandomAssType();
+        AssFrames = Assets.GetAssFrames(CurrentAssType);
+    }
+
+    async Task LoadDataAsync(string username)
+    {
+        assesEaten = await _localstorage.GetItemAsync<int>($"{username}_assesEaten");
+    }
+
+    void ResetGame()
+    {
+        assesEaten = 0;
+        piecesEaten = 0;
+        GameTimeInSeconds = 60;
+    }
+
+    void StartGame()
+    {
+        ResetGame();
+
+        GetNewAss();
+        GameTimer = new Timer(1000);
+        GameTimer.Elapsed += OnTimerTick;
+        GameTimer.AutoReset = true;
+        GameTimer.Enabled = true;
+
+        gamePlaying = true;
+    }
+
+    void OnTimerTick(object sender, EventArgs e)
+    {
+        if (GameTimeInSeconds > 0)
+        {
+            GameTimeInSeconds--;
+            InvokeAsync(() => StateHasChanged());
+            return;
+        }
+
+        GameTimer.Stop();
+        gamePlaying = false;
+
+        InvokeAsync(async () =>
+        {
+            await ShowResultsDialog();
+            StateHasChanged();
+        });
+    }
+
+    async Task SaveDataAsync(string username)
+    {
+        _localstorage.SetItemAsync($"{username}_assesEaten", assesEaten);
+    }
+
+    async Task EatPiece()
+    {
+        if (!gamePlaying)
+        {
+            StartGame();
+        }
+
+        if (piecesEaten == AssFrames.Count()-1)
+        {
+            piecesEaten = 0;
+            assesEaten += Assets.GetPointsForAssType(CurrentAssType);
+
+            Breakdown[CurrentAssType]++;
+            GetNewAss();
+        }
+        else
+        {
+            ++piecesEaten;
+        }
+        await js.InvokeVoidAsync("playSound", "/sounds/bite.mp3");
+    }
+
+    async Task ShowHelpDialog()
+    {
+        await HelpDialog?.ShowAsync();
+    }
+
+    async Task HideHelpDialog()
+    {
+        await HelpDialog?.HideAsync();
+    }
+
+
+    async Task ShowAboutDialog()
+    {
+        await AboutDialog?.ShowAsync();
+    }
+
+    async Task HideAboutDialog()
+    {
+        await AboutDialog?.HideAsync();
+    }
+
+    async Task ShowResultsDialog()
+    {
+        await ResultsDialog?.ShowAsync();
+    }
+
+    async Task TryAgain()
+    {
+        ResetGame();
+        await ResultsDialog?.HideAsync();
+    }
+
+    async Task HideResultsDialog()
+    {
+        await ResultsDialog?.HideAsync();
+    }
+}
---
+++ Shared/Assets.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Shared;
+
+public class Assets
+{
+    public enum AssTypeEnum
+    {
+        Boney,
+        Cartoon,
+        Flat,
+        Golden,
+        GYAT,
+        Hairy
+    }
+
+    public static Dictionary<string, List<string>> Frames = new Dictionary<string, List<string>>()
+    {
+        { 
+            AssTypeEnum.Boney.ToString(), new List<string> 
+            {
+                "entire_ass.png",
+                "chunk1.png",
+                "chunk2.png",
+                "chunk3.png",
+                "chunk4.png",
+            } 
+        },
+
+        { 
+            AssTypeEnum.Cartoon.ToString(), new List<string> 
+            {
+                "entire_ass.png",
+                "chunk1.png",
+                "chunk2.png",
+                "chunk3.png",
+                "chunk4.png",
+                "chunk5.png",
+                "chunk6.png",
+                "chunk7.png",
+                "chunk8.png",
+                "chunk9.png",
+                "chunk10.png",
+                "chunk11.png",
+                "chunk12.png",
+                "hole.png",
+            } 
+        },
+        { 
+            AssTypeEnum.Flat.ToString(), new List<string> 
+            {                 
+                "entire_ass.png",
+                "chunk1.png",
+                "chunk2.png",
+                "chunk3.png",
+                "chunk4.png",
+                "chunk5.png",
+                "chunk6.png",
+                "chunk7.png",
+                "chunk8.png",
+                "hole.png"
+            } 
+        },
+        { 
+            AssTypeEnum.Golden.ToString(), new List<string> 
+            { 
+                "entire_ass.png",
+                "chunk1.png",
+                "chunk2.png",
+                "chunk3.png",
+                "chunk4.png",
+                "chunk5.png",
+                "chunk6.png",
+                "chunk7.png",
+                "chunk8.png",
+                "chunk9.png",
+                "chunk10.png",
+                "chunk11.png",
+                "chunk12.png",
+                "chunk13.png",
+                "chunk14.png",
+                "chunk15.png",
+                "chunk16.png",
+                "chunk17.png",
+                "chunk18.png",
+                "chunk19.png",
+                "chunk20.png",
+                "chunk21.png",
+                "chunk22.png",
+                "chunk23.png",
+                "chunk24.png",
+                "chunk25.png",
+                "hole.png",
+                "hole.png",
+                "hole.png",
+                "hole.png",
+                "hole.png"
+            } 
+        },
+        { 
+            AssTypeEnum.GYAT.ToString(), new List<string> 
+            { 
+                "entire_ass.png",
+                "chunk1.png",
+                "chunk2.png",
+                "chunk3.png",
+                "chunk4.png",
+                "chunk5.png",
+                "chunk6.png",
+                "chunk7.png",
+                "chunk8.png",
+                "chunk9.png",
+                "chunk10.png",
+                "chunk11.png",
+                "chunk12.png",
+                "chunk13.png",
+                "hole.png"
+
+            } 
+        },
+        { 
+            AssTypeEnum.Hairy.ToString(), new List<string> 
+            { 
+                "entire_ass.png",
+                "chunk1.png",
+                "chunk2.png",
+                "chunk3.png",
+                "chunk4.png",
+                "chunk5.png",
+                "chunk6.png",
+                "chunk7.png",
+                "hole.png"
+            } 
+        }
+    };
+
+    public static AssTypeEnum GetRandomAssType()
+    {
+        Random random = new Random();
+
+        // Create a weighted list of enum values
+        var weightedValues = new[]
+        {
+            AssTypeEnum.Boney,
+            AssTypeEnum.Cartoon,
+            AssTypeEnum.Flat,
+            AssTypeEnum.GYAT,
+            AssTypeEnum.Hairy
+        };
+
+        // Add 99 normal values and 1 'Golden' to make it 1% chance for 'Golden'
+        var weightedList = new AssTypeEnum[50];
+        for (int i = 0; i < 49; i++)
+        {
+            weightedList[i] = weightedValues[random.Next(weightedValues.Length)]; // exclude 'Golden'
+        }
+        weightedList[49] = AssTypeEnum.Golden; // 1% chance
+
+        return weightedList[random.Next(weightedList.Length)];
+    }
+
+    public static List<string> GetAssFrames(AssTypeEnum assType)
+    {
+        return Frames[assType.ToString()];
+    }
+
+    public static double GetPointsForAssType(AssTypeEnum assType)
+    {
+        switch (assType)
+        {
+            case AssTypeEnum.Boney:
+                return 0.5;
+            case AssTypeEnum.Golden:
+                return 10;
+            case AssTypeEnum.GYAT:
+                return 2;
+            case AssTypeEnum.Cartoon:
+            case AssTypeEnum.Flat:
+            case AssTypeEnum.Hairy:
+            default:
+                return 1;
+        }
+    }
+}
